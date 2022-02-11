a88fddeb0e33a86df9fbda41f4942db8f25612ac
Zamal Hossain <thezamalian@gmail.com>
2022-02-11 12:45:16 +0600

    database collection is created

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000 ;
 
 app.use(cors());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xwjoa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+
+        const database = client.db('doctors_portal');
+        const appointmentCollection = database.collection('appointments');
+
+        app.post('/appointments', async (req, res) => {
+
+        });
+
+
+        console.log('Database connected successfully');
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
-  res.send('Hello Doctors Portal!')
-})
+  res.send('Hello Doctors Portal!');
+});
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
-})
+  console.log(`Listening on port ${port}`);
+});
+
+// app.get('/users');
+// app.get('/users/:id');
+// app.post('/users');
+// app.put('/users/:id');
+// app.delete('/users/:id');
